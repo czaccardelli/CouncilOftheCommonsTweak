6a6045681b94ff85cdb76a77646334ea88c19217
Carl Zaccardelli <czaccardelli@applied-inc.com>
2020-04-21 00:34:48 -0400

    Add project files.

---
+++ CouncilOftheCommonsTweak/CouncilOfTheCommonsTweakSubModule.cs
@@ -0,0 +1,15 @@
+﻿using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace CouncilOfTheCommonsTweak
+{
+    public class CouncilOfTheCommonsTweakSubModule : MBSubModuleBase
+    {
+
+        protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
+        {
+            base.OnGameStart(game, gameStarterObject);
+            gameStarterObject.AddModel(new TweakedCouncilOfTheCommonsModel());
+        }
+    }
+}
---
+++ CouncilOftheCommonsTweak/TweakedCouncilOfTheCommonsModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.SandBox.GameComponents;
+
+
+namespace CouncilOfTheCommonsTweak
+{
+    public class TweakedCouncilOfTheCommonsModel : DefaultClanPoliticsModel
+    {
+        public override float CalculateInfluenceChange(Clan clan, StatExplainer explanation = null)
+        {
+            float influenceChange = 0f;
+            if(clan.Kingdom != null)
+            {
+                clan.Kingdom.ActivePolicies.Remove(DefaultPolicies.CouncilOfTheCommons);
+                influenceChange = base.CalculateInfluenceChange(clan, explanation);
+                clan.Kingdom.ActivePolicies.Add(DefaultPolicies.CouncilOfTheCommons);
+            }    
+            return influenceChange;
+        }
+    }
+}
